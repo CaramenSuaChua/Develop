2f87237ca74148289fde89d9ac3c8a12234a8a1b
Caramen <caramen@Mac-mini-cua-Caramen.local>
2025-11-01 20:37:23 +0700

    update

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+# serializers.py
 from rest_framework import serializers
 from .models import Product
 
@@ -5,3 +6,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'price', 'created_at', 'updated_at']
+
+    def to_internal_value(self, data):
+        print("Raw data received:", data)  # Debug: xem dữ liệu nhận được
+        return super().to_internal_value(data)
